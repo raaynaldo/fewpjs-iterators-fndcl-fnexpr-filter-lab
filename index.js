8374752dab38386649d5fa2044ed1eca2d23dcf0
raaynaldo <raaynaldo@hotmail.com>
2020-11-16 18:26:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase() == name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver.slice(0, name.length) == name;
+  });
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver["name"] == name;
+  });
+}
